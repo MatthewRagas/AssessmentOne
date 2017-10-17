7611d35bb11824a57d941c088301a8ebead77467
Matthew Ragas <mjrag310@gmail.com>
2017-10-17 18:58:41 -0500

    AssesmenOne files

--- AssessmentOne/Wumpus/Main.cpp
+++ AssessmentOne/Wumpus/Main.cpp
@@ -1,8 +1,5 @@
 #include<iostream>
 #include"Dungeon.h"
-#include"Player.h"
-#include"Point2D.h"
-#include"PittyWumpus.h"
 #include "MyString.h"
 #include"time.h"
 using namespace std;
@@ -11,38 +8,21 @@ int main()
 {
 	//-------------------------------Variables-----------------------------------------
 	srand(time(NULL));
-	int lives = 3;
 	char input;
-	MyString name;
-	Player* billy = new Player();
-	Point2D* rooms = new Point2D();
-	Dungeon cave(9,9, billy);
-	
-	PittyWumpus beast[4];//code for setting the wumpi to the grid
-	for (int i = 0; i < 4; i++)
-	{
-		beast[i].SetPosition(rand() % 7, rand() % 7);
-		if (i > 0)
-		{
-			if ((beast[i].GetPosition().GetX() == beast[i - 1].GetPosition().GetX()) &&
-				(beast[i].GetPosition().GetY() == beast[i - 1].GetPosition().GetY()))
-			{
-				i--;
-			}
-		}
-	}
-	
+	MyString name;	
+	Player* treasure = new Player();
+	Dungeon cave;
 	//-------------------------------Game introduction---------------------------------
 	cout << "Welcome to Wumpus World!" << endl;
 	cout << "What you are looking at is the size of the dungeon you will be exploring.\n";
 	cout << "You will start in the room marked <0,0>.\n";
 	system("pause");
-	system("CLS");
-	cout << "Currently your chracter's name is little Billy.\n" << " If you wish to change it, input the name now.\n"
-		<<"If you do not wish to change it type 'No'.\n"
+	
+	cout << "Currently your chracter's name is little Billy.\n" << "If you wish to change it, input the name now.\n"
+		<< "If you do not wish to change it type 'NO'.\n"
 		<< endl;
 	cin >> name;
-	if (name == MyString("No"))
+	if (name == MyString("NO"))
 	{
 		name = "little Billy";
 	}
@@ -58,9 +38,9 @@ int main()
 	cout << "You have to watch out for wumpi and pitts though.\n";
 	system("pause");
 	system("CLS");
-	cout << "If you come to a room that is near a wumpus, the game will warn you with\n"; 
+	cout << "If you come to a room that is near a wumpus, the game will warn you with\n";
 	cout << "'You smell something foul'.\n";
-	cout << "If you come near a pit the game will warn you with\n"; 
+	cout << "If you come near a pit the game will warn you with\n";
 	cout << "'you feel a warm breeze'.\n";
 	cout << "May the odds be ever in your favor " << name << "!\n";
 	system("pause");
@@ -68,36 +48,31 @@ int main()
 	cave.GenRooms();
 	cout << "keep in mind that you start at <0,0>, the top left of the grid.\n";
 	cout << "So going up or left on your first move will\n";
-	cout << "cause you to fall off the map and lose a life. Oh yea don't fall off the map\n"; 
+	cout << "cause you to fall off the map and lose a life. Oh yea don't fall off the map\n";
 	cout << "either for reasons recently revealed. You have 3 lives in total.\n";
 	system("pause");
 	//---------------------------------Game Loop------------------------------------------	
 	system("CLS");
-	cout << "The room is dark. You cant see much.\n";
-	while (lives > 0)
+	cout << "The room is dark. You can't see much.\n";
+	system("pause");
+	int p = 1;
+	while (cave.GetPlayer().Lives() > 0)
 	{
-		cin >> input;
-		billy->MovePlayer(input);
-		cave.CheckPlayerPosition();
-		for (int i = 0; i < 4; i += 2)
-		{
-			if ((billy->GetPosition().GetX() - beast[i].GetPosition().GetX() == 1 || billy->GetPosition().GetX() - beast[i].GetPosition().GetX() == -1) &&
-				(billy->GetPosition().GetY() - beast[i].GetPosition().GetY() == 1 || billy->GetPosition().GetY() - beast[i].GetPosition().GetY() == -1))
-			{//checks if the player is near a wumpus.
-				cout << "You smell something foul.\n";
-			}
-		}
-		for (int i = 1; i < 4; i += 2)
-		{//checks if the player is near a pit.
-			if ((billy->GetPosition().GetX() - beast[i].GetPosition().GetX() == 1 || billy->GetPosition().GetX() - beast[i].GetPosition().GetX() == -1) &&
-				(billy->GetPosition().GetY() - beast[i].GetPosition().GetY() == 1 || billy->GetPosition().GetY() - beast[i].GetPosition().GetY() == -1))
-				{
-					cout << "You feel a warm breeze.\n";
-				}
-		}
+		system("CLS");
 		
+		
+		while (p == 1)
+		{
+			cout << "You are now free to move about the cave.\n";
+			p++;
+		}
+		cin >> input;
+		
+		cave.CheckPlayerPosition(input);
+		
+		system("pause");
 	}
-
-
+	cout << "You are out of lives.\n                ....GAME OVER....\n";
+	system("CLS");
 	system("pause");
 }
